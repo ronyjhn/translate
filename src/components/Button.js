4667d9b5629c875cc6d4fa53b2c167b3c39db579
Rony John <ronyjhn@gmail.com>
2020-04-05 23:02:23 +0530

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+class Button extends Component {
+	renderText(language) {
+		return language === 'english' ? 'Submit' : 'Voorleggen';
+	}
+
+	renderButton(color) {
+		return (
+			<button className={`ui button ${color}`}>
+				<LanguageContext.Consumer>{({ language }) => this.renderText(language)}</LanguageContext.Consumer>
+			</button>
+		);
+	}
+
+	render() {
+		return <ColorContext.Consumer>{(color) => this.renderButton(color)}</ColorContext.Consumer>;
+	}
+}
+export default Button;
---
+++ src/components/UserCreate.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Button from './Button';
+import Field from './Field';
+
+class UserCreate extends Component {
+	render() {
+		return (
+			<div className="ui form">
+				<Field />
+				<Button />
+			</div>
+		);
+	}
+}
+export default UserCreate;
